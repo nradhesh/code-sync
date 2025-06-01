841943ab78a515f95ef3812c10132f91209a8bd2
nradhesh <136627964+nradhesh@users.noreply.github.com>
2025-06-01 23:14:34 +0530

    test

--- server/src/__tests__/notfound.test.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import { app } from '../server';
-
-describe('Not Found Handler', () => {
-  it('GET /unknown should return 404', async () => {
-    const response = await request(app).get('/unknown');
-    expect(response.status).toBe(404);
-  });
-}); 
